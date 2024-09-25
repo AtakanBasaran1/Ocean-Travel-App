da013526d2d3d2c820f2f211305c5f5cb2048012
Atakan <158854874+Atakanlift@users.noreply.github.com>
2024-09-25 21:37:07 +0300

    g
    g

--- app/src/app/page.jsx
+++ app/src/app/page.jsx
@@ -1,105 +1,31 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import aside from './images/aside.png'
 import dogfish from './images/dogfish.png'
 
 const Page = () => {
-
-
   return (
-    <div className='container'>
+    <div className='min-h-screen w-full  '>
+      <img src={aside.src} alt="" className="w-full h-full z-10 object-cover fixed" />
 
-      <aside>
-        <h1 className='text-white font-semibold p-5 text-[28px] flex text-center mt-[60px]'>Deniz Altının Gizemli Dünyasını Keşfetmeye Hazır mısınız?</h1>
+      <div className='z-50 absolute'>
+        <h1 className='mt-[150px] flex justify-start ml-10 text-[42px] text-gray-300'>AQUA</h1>
 
-        <p className='text-white text-[18px]  p-5 mt-10 flex text-center'>Bir ömür boyu unutamayacağınız bir maceraya atılın! Türkiye’nin en güzel su altı noktalarında, profesyonel rehberler eşliğinde unutulmaz bir yolculuğa çıkın. Şimdi yerinizi ayırtın ve deniz altının büyüleyici dünyasını keşfedin.</p>
+        <p className='ml-10 flex justify-start w-[350px] text-[18px] mt-10 text-gray-300'>Bir ömür boyu unutamayacağınız bir maceraya atılın! Türkiye’nin en güzel su altı noktalarında, profesyonel rehberler eşliğinde unutulmaz bir yolculuğa çıkın. Şimdi yerinizi ayırtın ve deniz altının büyüleyici dünyasını keşfedin.</p>
 
-        <h2 className='text-white text-[20px] flex justify-center p-5'>Hemen Rezervasyon Yap</h2>
+        <img
+          className='absolute flex justify-end mt-[-50px] w-[800px] h-[300px]'
+          src={dogfish.src} alt="" />
 
-        <div className="grid h-screen place-items-center mb-[-600px]">
-          <button className='bg-blue-400 mt-[-700px] hover:bg-blue-600 hover:bg-opacity-50 bg-opacity-30 w-[250px] h-[40px] text-white rounded-[30px]'>
-            <p className='text-[14px] mt-3'>Rezervasyon Yap</p>
-          </button>
+        <div className='flex justify-center'>
+          <button
+            className='bg-white w-[300px] h-[60px] mt-[350px] hover:bg-gray-300  rounded-[10px]'
+          ><p className='text-blue-600 text-[20px] mt-3 font-semibold'>Hemen Başlayın</p></button>
         </div>
-
-
-
-      </aside>
-
-      <hr className='border-blue-300 mt-[30px]' />
-
-      <bside>
-        <h1 className='text-white p-5 flex text-center'>Neden Bizimle Keşfetmelisiniz?</h1>
-
-        <img src={dogfish.src} className='w-[400px] mt-[-40px] ml-[-10px] mb-5' alt="" />
-
-        <ul className='text-white'>
-
-          <div className='flex text-start justify-start p-5 flex-col'>
-            <li className='text-[20px] mb-5'>Eşsiz Bir Deneyim</li>
-            <p className='text-[17px]'>Deniz altının renkli ve gizemli dünyasında yolculuk yaparak su altı canlılarıyla tanışma fırsatı.</p>
-
-            <h3 className='text-[19px]'>Hangi Canlılarla Karşılaşacağım?</h3>
-            <button className='bg-blue-400 hover:bg-blue-600 hover:bg-opacity-50 bg-opacity-30 w-[200px] h-[35px] mt-7 text-white rounded-[30px] '><p className='text-[14px] justify-center flex mt-3'>Detaylara Git</p></button>
-            <hr className='border-blue-300' />
-          </div>
-
-          <div className='flex text-start justify-start p-5 flex-col'>
-            <li className='text-[20px] mb-5'>Güvenlik Önceliğimizdir</li>
-            <p className='text-[17px]'>Tüm ekipmanlarımız en son teknoloji ile donatılmış ve rehberlerimiz profesyonel dalış eğitmenleridir.</p>
-            <hr className='border-blue-300' />
-          </div>
-
-          <div className='flex text-start justify-start p-5 flex-col'>
-            <li className='text-[20px] mb-5'>Her Yaşa Uygun</li>
-            <p className='text-[17px]'>İster yeni başlayan, ister deneyimli bir dalgıç olun, her seviyeye uygun turlarımız ile size en iyi deneyimi sunuyoruz.</p>
-            <hr className='border-blue-300' />
-          </div>
-
-        </ul>
-
-      </bside>
-
-      <cside className='flex justify-center text-center items-center'>
-        <h1 className='text-white p-5'>Tur Paketlerimiz</h1>
-
-
-        <div className='mt-10 absolute mt-[880px]'>
-          <div class="cards">
-            <div class="card red">
-              <p class="tip">Başlangıç Seviye Turu
-
-              </p>
-              <p class="second-text">Süre: 2 saat</p>
-              <p class="second-text">Fiyat: 300₺</p>
-              <p class="second-text">İçerik: Kısa su altı turu, temel dalış eğitimi.
-              </p>
-              <button className='bg-blue-400 mt-3 w-[150px] h-[35px] rounded-[30px] hover:bg-blue-600 '><p className='mt-[7px] text-white hover:text-white'>Detayları İncele</p></button>
-            </div>
-            <div class="card blue">
-              <p class="tip">Keşif Turu</p>
-              <p class="second-text">Süre: 2 saat</p>
-              <p class="second-text">Fiyat: 300₺</p>
-              <p class="second-text">İçerik: Kısa su altı turu, temel dalış eğitimi.
-              </p>
-              <button className='bg-blue-400 mt-3 w-[150px] h-[35px] rounded-[30px] hover:bg-blue-600 '><p className='mt-[7px] text-white hover:text-white'>Detayları İncele</p></button>
-            </div>
-            <div class="card green">
-              <p class="tip">Profesyonel Seviye Turu</p>
-              <p class="second-text">Süre: 2 saat</p>
-              <p class="second-text">Fiyat: 300₺</p>
-              <p class="second-text">İçerik: Kısa su altı turu, temel dalış eğitimi.
-              </p>
-              <button className='bg-blue-400 mt-3 w-[150px] h-[35px] rounded-[30px] hover:bg-blue-600 '><p className='mt-[7px] text-white hover:text-white'>Detayları İncele</p></button>
-            </div>
-          </div>
-        </div>
-
-
-      </cside>
-
-
+      </div>
     </div>
   )
 }
-export default Page
+
+export default Page;
